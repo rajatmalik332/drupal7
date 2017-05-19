18876d9c54de8e1b40b1f2069c7819195812c313
rajatmalik332 <rajat.malik@brihaspatitech.com>
2017-05-19 19:23:39 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -20,3 +20,9 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
+
+
+
+
+
+# new file
